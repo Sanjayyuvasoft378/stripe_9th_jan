fc749a5f40a7ccfd52941b8806f1fff8ce78b931
Sanjayyuvasoft378 <sanjayyuvasoft378@gmail.com>
2023-01-09 18:23:54 +0530

    stripe_product_crud

---
+++ services/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import *
+# Register your models here.
+# @admin.register(Course)
+# class CourseAdmin(admin.ModelAdmin):
+#     list_display = ['course_name','course_description','is_premium','course_image']
